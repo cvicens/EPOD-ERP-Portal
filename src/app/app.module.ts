6f2818cba4d7fbda337a08f51ef4655bce90f6a0
Carlos Vicens <carlos.vicens.alonso@gmail.com>
2017-04-24 11:50:02 +0200

    new order ready to be used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CreateOrderComponent } from './create-order/create-order.component';
+
+import { FHService } from './service/fh.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateOrderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'orders',
+        component: CreateOrderComponent
+      }
+    ])
   ],
-  providers: [],
+  providers: [FHService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/model/form-order.ts
@@ -0,0 +1,7 @@
+export class FormOrder {
+  constructor(
+    public id: string,
+    public title: string,
+    public assignee: string
+  ) {  }
+}
